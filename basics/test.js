bedb73059f3134fbc3bccb7ffaba9aadf4606a80
Anuj-Kr-Tomar <anujtomar8258@gmail.com>
2025-01-16 19:03:12 +0000

    codjs

---
+++ basics/test.js
@@ -0,0 +1,9 @@
+const accountnum = 12345
+let accountmail = "anuj@1234.com"
+accountcity = "jaipur"
+
+// accountnum = 1234
+accountmail = "anuskhj@1234.com"
+accountcity = "jaipuroijh"
+
+console.table([accountnum, accountmail,accountcity])
